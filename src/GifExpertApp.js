c473652ddc605a556ca31146caa53a49c14dbd00
Jimmy Salto <saltojimmy@gmail.com>
2020-11-30 23:16:33 -0500

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState( []);
+    /* 
+    const handleAdd = () =>{
+         setCategories(
+             [...categories,'Naruto']
+         );
+    } */
+
+    return (
+        <>
+            <h2> 
+                GifExpertApp
+            </h2>
+            <hr/>   
+            <AddCategory setCategories={ setCategories }/>
+
+            <ol>
+                {
+                    categories.map(
+                        category => (<GifGrid 
+                            key={category}
+                            category={category}/> )
+                         
+                    )
+                }
+            </ol>
+        </>
+    )
+}
